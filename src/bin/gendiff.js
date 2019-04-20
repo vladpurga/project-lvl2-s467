6c070cc2ffb1f8fc3203c4e76defedc25db51afb
vladpurga <vladpurga@my.com>
2019-04-20 16:45:08 +0300

    show help

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,4 +2,11 @@
 
 // import diff from '..';
 
-console.log('hello!');
+const program = require('commander');
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-f, --format [type]', 'Output format')
+  .parse(process.argv);
